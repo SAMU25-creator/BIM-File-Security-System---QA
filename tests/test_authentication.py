11b820b29b97610fd31edf60806c7d2c4a438b9b
SAMU25_creator <samukelosofala@gmail.com>
2026-08-27 21:09:22 +0200

    added the authentication test for wrond password rejection and it passed

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -10,3 +10,9 @@ def test_invalid_username_rejected():
     result = login("invalid_user", "correct_password")
 
     assert result is False
+
+def test_wrong_password_rejected():
+    result = login("valid_user", "wrong_password")
+
+    assert result is False 
+
