1a6f6df3b267639b9dc86cdf8f53ebb93065ad0c
SAMU25_creator <samukelosofala@gmail.com>
2026-08-27 21:04:17 +0200

    added the authentication test for invalid user and it passed

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -5,3 +5,8 @@ def test_valid_user_correct_password():
     result = login("valid_user", "correct_password")
 
     assert result is True
+
+def test_invalid_username_rejected():
+    result = login("invalid_user", "correct_password")
+
+    assert result is False
